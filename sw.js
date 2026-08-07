ac8cf80a423dd5c2105fa158c3e0dfd1cbbec1d5
Snivek93 <ksoto9393@gmail.com>
2026-08-07 07:12:30 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * Eso obliga a los teléfonos/computadoras que ya tienen la app instalada a
  * bajar los archivos nuevos en vez de seguir usando los viejos guardados.
  */
-const CACHE_VERSION = "v9";
+const CACHE_VERSION = "v10";
 const CACHE_NAME = `cortafuego-hilti-${CACHE_VERSION}`;
 
 const ARCHIVOS_PRECACHE = [
